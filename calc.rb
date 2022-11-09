6e4e8f6cc18cd643653f62d4df2c976cc10cc013
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-11-09 09:44:16 +0300

    Adds the operator symbol

--- calc.rb
+++ calc.rb
@@ -1,6 +1,24 @@
 puts "Enter first number:"
-number1=gets.chomp()
-puts "Enter second number:"
-number2=gets.chomp()
+number1=gets.chomp().to_f
 
-puts (number1.to_i+number2.to_i)
+puts "Enter Operator"
+op=gets.chomp()
+
+puts "Enter second number:" 
+number2=gets.chomp().to_f
+
+if op == "+"
+    puts (number1+number2)
+
+elsif op =="-"
+    puts (number1-number2)
+
+elsif op == "/"
+    puts (number1 / number2)
+
+elsif op == "*"
+    puts (number1*number2)
+else 
+    puts "Invalid operation!"
+end 
+
